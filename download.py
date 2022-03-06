8426222da6c71e3ab56802e5286fc186e247dd66
Indosaram <freedom_zero@naver.com>
2022-03-06 09:36:54 +0900

    add temp folder

--- download.py
+++ download.py
@@ -13,11 +13,16 @@ import pandas as pd
 
 
 class ArchivedReddit:
-    def __init__(self, data_dir="data"):
-        self.data_dir = os.path.join(os.getcwd(), data_dir)
+    def __init__(self):
+        self.data_dir = os.path.join(os.getcwd(), "data")
+        self.temp_dir = os.path.join(os.getcwd(), "temp")
 
-    @staticmethod
-    def _process_json(filename):
+        folders = ["data", "result", "temp"]
+        for folder in folders:
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+
+    def _process_json(self, filename):
         """
         archived json file has each line of json data, therefore first parse by
         line and load a line to dictionrary."""
@@ -25,7 +30,7 @@ class ArchivedReddit:
         print(f"Start processing {filename}")
         temp_dict = {'subreddit': [], 'title': [], 'selftext': [], 'url': []}
 
-        with open(filename, "r") as file:
+        with open(os.path.join(self.temp_dir, filename), "r") as file:
             for line in file:
                 data = json.loads(line)
 
@@ -40,13 +45,13 @@ class ArchivedReddit:
             print(f"Converting {filename} to dataframe")
             df = pd.DataFrame(temp_dict)
             print(f"Saving {filename} to csv file")
-            df.to_csv(f"{filename}.csv")
+            df.to_csv(os.path.join(self.data_dir, f"{filename}.csv"))
         print(f"Finish processing {filename}")
 
     def process_files(self):
         filenames = [
-            os.path.join(self.data_dir, filename)
-            for filename in os.listdir(self.data_dir)
+            filename
+            for filename in os.listdir(self.temp_dir)
             if not filename.endswith('.csv')
         ]
 
@@ -56,7 +61,7 @@ class ArchivedReddit:
     def download_file(self, url):
         file_name = re.search("(?:.*\/)+(.*\..*)", url).group(1)
         self.execute(
-            f"wget -4 -O {os.path.join(self.data_dir,file_name)} {url}"
+            f"wget -4 -O {os.path.join(self.temp_dir,file_name)} {url}"
         )
 
     @staticmethod
@@ -80,25 +85,26 @@ class ArchivedReddit:
         # Overwite existing file, remove source file
         command = (
             "zstd -f --rm --memory=2048MB  --decompress"
-            f" {os.path.join(self.data_dir,filename)}"
+            f" {os.path.join(self.temp_dir,filename)}"
         )
         self.execute(command)
 
     def decompress(self):
         filenames = [
-            os.path.join(self.data_dir, filename)
-            for filename in os.listdir(self.data_dir)
+            os.path.join(self.temp_dir, filename)
+            for filename in os.listdir(self.temp_dir)
             if filename.endswith('.zst')
         ]
 
         for file_name in filenames:
             self._decompress(file_name)
 
+
 if __name__ == "__main__":
     archived_reddit = ArchivedReddit()
 
-    min_month = "2020-04"
-    max_month = "2020-05"
+    min_month = "2005-06"
+    max_month = "2005-07"
 
     dates = pd.period_range(min_month, max_month, freq='M')
 
@@ -110,7 +116,7 @@ if __name__ == "__main__":
     num_cpu = cpu_count()
     print("There are {} CPUs on this machine ".format(num_cpu))
 
-    print("Start downloading files")
+    print("Start downloading and decompressing files")
     pool = Pool(min(len(uris), num_cpu))
     pool.map(partial(archived_reddit.download_file), uris)
     pool.close()
